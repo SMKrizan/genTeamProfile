8d6459c1be3187375a1c8d7dde621adeb568e288
SMKrizan <smkrizan@gmail.com>
2020-11-05 16:02:01 -0600

    data flow prepped

--- index.js
+++ index.js
@@ -1,3 +1,95 @@
-const teamRoster = require('./lib/teamRoster.js');
+// access npm modules
+const inquirer = require('inquirer');
+// access file generation dependencies
+const generateHTML = require('./src/roster-template.js');
+const { writeFile } = require('../utils/generateRoster.js');
+// access object modules
+const Employee = require('./lib/Employee');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+const Manager = require('./lib/Manager');
 
-new teamRoster().initializeTeam();
+const teamData = [];
+
+
+function Team() {
+    this.teamMembers = {};
+}
+
+// gather roster content through series of questions to user
+const initializeTeam = () => {
+    console.log(`
+    ===================================================
+    Answer these questions to set up a new team roster:
+    ===================================================
+    `)
+
+    return 
+}
+
+class Engineer {
+    inquirer
+    .prompt({
+        type: 'input',
+        name: 'name',
+        message: "Enter the team member's name: "
+    })
+    .then (({ name }) => {
+        this.employee = new Employee(name);
+    })
+}
+
+initializeTeam.prototype.id = function() {
+    inquirer
+    .prompt({
+        type: 'number',
+        name: 'id',
+        message: "Enter the team member's employee id:  "
+    })
+    .then (({ id }) => {
+        this.employee.id = new Employee.id(id);
+    })
+}
+
+initializeTeam.prototype.email = function() {
+    inquirer
+    .prompt({
+        type: 'input',
+        name: 'email',
+        message: "Enter the team member's work email: "
+    })
+    .then (({ email }) => {
+        this.employee.email = new Employee.email(email);
+    })
+}
+
+initializeTeam.prototype.role = function() {
+    inquirer
+    .prompt({
+        type: 'list',
+        name: 'role',
+        message: "Enter the team member's role: ",
+        choices: ['employee', 'engineer', 'manager', 'intern']
+    })
+    .then (({ role }) => {
+        this.employee.role = new Employee.role(role);
+    })
+}
+
+// the input-gathering function is called in order to prompt its output to the file-generating function
+initializeTeam()
+    // the output function 'teamData' is sent to the 'generateRoster' function
+    .then(teamData => {
+        // returns generateRoster(teamData) to file creation;
+        const manager = new Manager(teamData.managername, teamData.managerId, teamData.manager.Email, teamData.office);
+        team.push(manager)
+    })
+    // a file will then be genereated, 'teamRoster.html', within which the html template code will be actuated
+    .then(htmlFile => {
+        return writeFile(htmlFile);
+    })
+    .catch(err => {
+        console.log(err);
+    });
+
+
---
+++ utils/generateRoster.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+
+// output for this function is creation of a new HTML file, 'teamRoster.html'
+
+    // if there is an error, related message will be sent to Promise's 'catch()' method
+
+    // return out of present function to ensure the Promise is not inadvertantly passed to the 'resolve()' function
+
+// OR otherwise, Promise is sent to 'resolve()' and data returned to '.then()' method
+
+// resolved data is exported to newly generated file
+module.exports = { writeFile, copyFile };
